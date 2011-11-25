97dc04ecf48ecc4afb88100c205ef46ad1d19ab7
Thomas Smith <thomathom@gmail.com>
2011-11-25 17:49:56 -0500

    a tiny bit of stuff

---
+++ t9999-git-sub.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+test_description="git sub"
+#. ./test-lib.sh
+. ./lib-git-svn.sh
+
+test_expect_success 'setup git-svn' '
+	svnadmin load "$rawsvnrepo" < ${TEST_DIRECTORY}/t9999/svn.dump
+	git svn init "$svnrepo"
+	git svn fetch
+'
+
+test_expect_success 'git sub commit-from-svn' '
+	git sub commit-from-svn refs/remotes/git-svn^^
+	FIRST=`git rev-parse from-svn`
+	test -n "$FIRST"
+	git sub commit-from-svn refs/remotes/git-svn^
+	SECOND=`git rev-parse from-svn`
+	test -n "$SECOND"
+	#test "$FIRST" = `git rev-parse from-svn^`
+'
+
+test_expect_success 'git sub init' '
+	git svn info
+	git sub init
+	git branch -a | grep for-svn
+	NUM_DISTINCT=$(git rev-parse refs/remotes/git-svn from-svn for-svn | sort | uniq | wc -l)
+	test 2 -eq "$NUM_DISTINCT"
+'
+
+test_expect_success 'git sub unknown command' '
+	test_must_fail git sub blahblah foo
+'
+
+test_done
+
