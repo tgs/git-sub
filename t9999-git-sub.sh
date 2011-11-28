504342313ee815056dfc36d7eab6fd1f795626d4
Thomas Grenfell Smith <tgs@resc.net>
2011-11-28 08:56:10 -0500

    Why won't that test fail

--- t9999-git-sub.sh
+++ t9999-git-sub.sh
@@ -12,7 +12,13 @@ test_expect_success 'setup git-svn' '
 '
 
 test_expect_success 'git sub commit-from-svn' '
+	# wtf wtf wtf wtf???
+	false
+	#test_must_fail git sub read-note-pointer refs/remotes/git-svn^^
 	git sub commit-from-svn refs/remotes/git-svn^^
+	#git sub read-note-pointer from-svn
+	#git sub read-note-pointer refs/remotes/git-svn^^
+	false
 	FIRST=`git rev-parse from-svn`
 	test -n "$FIRST"
 	git sub commit-from-svn refs/remotes/git-svn^
@@ -21,7 +27,7 @@ test_expect_success 'git sub commit-from-svn' '
 	#test "$FIRST" = `git rev-parse from-svn^`
 '
 
-test_expect_success 'git sub init' '
+test_expect_failure 'git sub init' '
 	git svn info
 	git sub init
 	git branch -a | grep for-svn
